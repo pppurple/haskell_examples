be18476f2b9d66f24b81f27b495e9678ebbb1de6
purple <shuggie.otis0@gmail.com>
2017-05-31 23:07:48 +0900

    add example

---
+++ basic/1-4_range.hs
@@ -0,0 +1,19 @@
+range20 = [1..20]
+
+chars = ['a'..'z']
+
+step2 = [2,4..20]
+
+step3 = [3,6..20]
+
+step13 = [13,26..24*13]
+
+step13take = take 24 [13,26..]
+
+cycle10 = take 10 (cycle [1,2,3])
+
+cycleLOL = take 12 (cycle "LOL ")
+
+repeat5 = take 10 (repeat 5)
+
+replicate10 = replicate 3 10
