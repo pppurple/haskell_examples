a58c0660aa164a3c8f6071fd7124887b992862d8
purple <shuggie.otis0@gmail.com>
2017-07-03 02:29:18 +0900

    add examples

---
+++ basic/5-1_curry.hs
@@ -0,0 +1,16 @@
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
+
+multTwoWithNine = multThree 9
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred x = compare 100 x
+
+compareWithHundred' :: Int -> Ordering
+compareWithHundred' = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
