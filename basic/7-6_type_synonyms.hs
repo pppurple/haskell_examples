976d2b5f5e38a8ea38925050c5a5882f35f34a7a
purple <shuggie.otis0@gmail.com>
2017-08-26 22:18:15 +0900

    add examples

---
+++ basic/7-6_type_synonyms.hs
@@ -0,0 +1,7 @@
+-- type PhoneBook = [(String,String)]
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
