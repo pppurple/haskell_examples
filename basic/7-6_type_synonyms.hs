53c996547377623b8b17c96dd29e1f9cc0189178
pppurple <shuggie.otis0@gmail.com>
2017-09-05 01:20:18 +0900

    update

--- basic/7-6_type_synonyms.hs
+++ basic/7-6_type_synonyms.hs
@@ -1,3 +1,4 @@
+import qualified Data.Map as Map
 -- type PhoneBook = [(String,String)]
 type PhoneNumber = String
 type Name = String
@@ -5,3 +6,20 @@ type PhoneBook = [(Name, PhoneNumber)]
 
 inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
 inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+
+phoneBook :: [(String, String)]
+phoneBook =
+    [("betty", "555-5555")
+    , ("bonnie", "444-4444")
+    , ("pasty", "333-3333")
+    , ("lucille", "222-2222")
+    , ("wendy", "666-6666")
+    , ("penny", "777-7777")
+    ]
+
+-- Either example
+data LockerState = Taken | Free deriving (Show, Eq)
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+
